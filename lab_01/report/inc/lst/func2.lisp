7c463234a89fa69453a0ec0d9c0939154aeb3568
mshsvnv <mshsvnv@mail.ru>
2024-02-13 19:09:31 +0300

    add lab_01

---
+++ lab_01/report/inc/lst/func2.lisp
@@ -0,0 +1,3 @@
+((lambda (ar1 ar2) (list '(ar1) '(ar2))) "a" "b")
+
+(defun f1 (ar1 ar2) (list (list ar1) (list ar2)))
---
+++ lab_01/report/inc/lst/func3.lisp
@@ -0,0 +1,3 @@
+((lambda (arl) (list (list (list arl)))) "a")
+
+(defun f1 (arl) (list (list (list arl))))
