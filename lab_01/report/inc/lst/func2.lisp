47d5afc91ad0022fce2b826d344ef7852121fdbf
mshsvnv <mshsvnv@mail.ru>
2024-02-19 00:00:16 +0300

    some fixikie

--- lab_01/report/inc/lst/func2.lisp
+++ lab_01/report/inc/lst/func2.lisp
@@ -1,3 +1,5 @@
-((lambda (ar1 ar2) (list '(ar1) '(ar2))) "a" "b")
+((lambda (ar1 ar2) (list (list ar1) (list ar2))) "a" "b")
+((lambda (ar1 ar2) (cons (cons ar1 Nil) (cons (cons ar2 Nil) Nil))) "a" "b")
 
 (defun f1 (ar1 ar2) (list (list ar1) (list ar2)))
+(defun f1 (ar1 ar2) (cons (cons ar1 Nil) (cons (cons ar2 Nil) Nil)))
--- lab_01/report/inc/lst/func3.lisp
+++ lab_01/report/inc/lst/func3.lisp
@@ -1,3 +1,5 @@
 ((lambda (arl) (list (list (list arl)))) "a")
+((lambda (arl) (cons (cons (cons arl Nil) Nil) Nil)) "a")
 
 (defun f1 (arl) (list (list (list arl))))
+(defun f1 (arl) (cons (cons (cons arl Nil) Nil) Nil))
